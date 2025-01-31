eaeb8572557b9b122541bbb12034295b3de4da18
Ppm19 <pablo19ppm@gmail.com>
2025-01-31 12:08:26 +0100

    ultimo6

--- api/index.py
+++ api/index.py
@@ -15,31 +15,44 @@ def get_alumnos():
     alumnos = list(alumnos_collection.find({}, {"_id": 1, "nombre": 1, "apellido": 1, "telefono": 1}))
     return jsonify(alumnos_schema.dump(alumnos))
 
-
 @app.route('/api/alumnos/<alumno_id>', methods=['GET'])
 def get_alumno_by_id(alumno_id):
     try:
-        alumno = alumnos_collection.find_one({"_id": ObjectId(alumno_id)})
+        alumno_id = int(alumno_id)
+        alumno = alumnos_collection.find_one({"id": alumno_id})
+        
         if not alumno:
             return jsonify({"error": "Alumno no encontrado"}), 404
+        
         return jsonify(alumno_schema.dump(alumno))
-    except:
+    except ValueError:
         return jsonify({"error": "ID inválido"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 @app.route('/api/alumnos', methods=['POST'])
 def add_alumno():
     try:
-        data = alumno_schema.load(request.get_json())
+        data = request.get_json()
+
+        if not all(k in data for k in ("nombre", "apellido", "telefono", "id")):
+            return jsonify({"error": "Todos los campos son necesarios."}), 400
+        if alumnos_collection.find_one({"id": data["id"]}):
+            return jsonify({"error": f"Ya existe un alumno con el id {data['id']}."}), 409
+        
         result = alumnos_collection.insert_one({
+            "id": data["id"],
             "nombre": data["nombre"],
             "apellido": data["apellido"],
             "telefono": data["telefono"]
         })
         
-        data["_id"] = str(result.inserted_id)
         return jsonify(data), 201
-    except ValidationError as err:
-        return jsonify(err.messages), 400
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run()
