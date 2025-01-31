aef27751c837ef4d9b4755655b400998239140a7
Ppm19 <pablo19ppm@gmail.com>
2025-01-31 12:34:35 +0100

    ultimo11

--- api/index.py
+++ api/index.py
@@ -8,18 +8,18 @@ CORS(app, origins="https://vercel-front-sage.vercel.app")
 
 alumnos_collection = db["alumnos"]
 
-# Obtener todos los alumnos
+
 @app.route('/api/alumnos', methods=['GET'])
 def get_alumnos():
     alumnos = list(alumnos_collection.find({}, {"_id": 0, "id": 1, "nombre": 1, "apellido": 1, "telefono": 1}))
     return jsonify(alumnos_schema.dump(alumnos))
 
-# Obtener un alumno por su id
+
 @app.route('/api/alumnos/<alumno_id>', methods=['GET'])
 def get_alumno_by_id(alumno_id):
     try:
-        print(f"Buscando alumno con id: {alumno_id}")  # Para depurar
-        alumno = alumnos_collection.find_one({"id": str(alumno_id)})  # Asegúrate de convertir a cadena
+        print(f"Buscando alumno con id: {alumno_id}")
+        alumno = alumnos_collection.find_one({"id": str(alumno_id)})
 
         if not alumno:
             return jsonify({"error": "Alumno no encontrado"}), 404
